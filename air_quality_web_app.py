a08e543f539b00056cbb60b2a66370d71b830b9e
Trevor Atkins <atkinstrevor03@gmail.com>
2024-06-01 10:09:16 -0400

    Update air_quality_web_app.py

--- air_quality_web_app.py
+++ air_quality_web_app.py
@@ -59,16 +59,43 @@ if st.button("Get Data"):
             st.subheader("Current Air Pollution Data")
             fig_current = px.scatter_mapbox(
                 df_current, lat='lat', lon='lon', size='main_aqi',
-                color='main_aqi', hover_data=['components_co', 'components_no', 'components_no2', 'components_o3', 'components_so2', 'components_pm2_5', 'components_pm10'],
-                zoom=10, height=300)
+                color='main_aqi', 
+                hover_data={
+                    'components_co': 'CO', 
+                    'components_no': 'NO', 
+                    'components_no2': 'NO2', 
+                    'components_o3': 'O3', 
+                    'components_so2': 'SO2', 
+                    'components_pm2_5': 'PM2.5', 
+                    'components_pm10': 'PM10'
+                },
+                zoom=10, height=300,
+                labels={'main_aqi': 'Air Quality Index', 'lat': 'Latitude', 'lon': 'Longitude'}
+            )
             fig_current.update_layout(mapbox_style="open-street-map", mapbox_accesstoken=mapboxkey)
             st.plotly_chart(fig_current)
             
             st.subheader("Forecast Air Pollution Data")
             df_forecast['dt'] = pd.to_datetime(df_forecast['dt'], unit='s')
             fig_forecast = px.line(
-                df_forecast, x='dt', y=['main_aqi', 'components_co', 'components_no', 'components_no2', 'components_o3', 'components_so2', 'components_pm2_5', 'components_pm10'],
-                labels={'value': 'Concentration', 'variable': 'Pollutant'})
+                df_forecast, x='dt', y=[
+                    'main_aqi', 'components_co', 'components_no', 'components_no2', 
+                    'components_o3', 'components_so2', 'components_pm2_5', 'components_pm10'
+                ],
+                labels={
+                    'value': 'Concentration', 
+                    'variable': 'Pollutant', 
+                    'dt': 'Date', 
+                    'main_aqi': 'Air Quality Index', 
+                    'components_co': 'CO', 
+                    'components_no': 'NO', 
+                    'components_no2': 'NO2', 
+                    'components_o3': 'O3', 
+                    'components_so2': 'SO2', 
+                    'components_pm2_5': 'PM2.5', 
+                    'components_pm10': 'PM10'
+                }
+            )
             st.plotly_chart(fig_forecast)
         else:
             st.error("Failed to retrieve data. Please check the inputs and try again.")
